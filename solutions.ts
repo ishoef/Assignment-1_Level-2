3000cde0c2ef86e3925c072169c5b5918edb7ce1
ishoef <ismailhossennayeb@gmail.com>
2025-11-16 19:32:09 +0200

    problem 2 solved

--- solutions.ts
+++ solutions.ts
@@ -1,11 +1,24 @@
 // Problem 1 Solving
-function formatValue(value: string | number | boolean) {
-  if (typeof value === "string") {
-    return value.toUpperCase();
-  } else if (typeof value === "number") {
-    return value * 10;
-  } else if (typeof value === "boolean") {
-    return !value;
-  }
+function formatValue(
+  value: string | number | boolean
+): string | number | boolean {
+  if (typeof value === "string") return value.toUpperCase();
+  if (typeof value === "number") return value * 10;
+  if (typeof value === "boolean") return !value;
+  return value;
 }
 
+// console.log(formatValue("hello"));
+// console.log(formatValue(5));
+// console.log(formatValue(true));
+
+// Problem 2 Solving
+
+function getLength(value: string | any[]): number {
+  if (typeof value === "string") return value.length;
+  if (Array.isArray(value)) return value.length;
+  return 0;
+}
+
+console.log(getLength("ismailnode solutions.ts"));
+console.log(getLength([10, 20, 30, 40]));
